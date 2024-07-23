ff34bcf0ec83edc7bf40aed75c7d13b643bc17ec
mr-rahul07 <rahul0406r@gmail.com>
2024-07-23 12:38:10 +0530

    Add files via upload

---
+++ student_details.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 23, 2024 at 06:30 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aiml`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student details`
+--
+
+CREATE TABLE `student details` (
+  `id` int(50) NOT NULL,
+  `First name` varchar(50) NOT NULL,
+  `Last name` varchar(10) NOT NULL,
+  `gender` enum('Male','Female','other') NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  `Phone Number` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student details`
+--
+ALTER TABLE `student details`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
